57a0c574239360e3ad9cbefa009f9bc0dd4a38a8
Amitanshu Varshney <amitanshu.varshney@gmail.com>
2025-11-29 19:58:02 +0530

    Created test folder and understood the directory

---
+++ Test Folder/src/App.jsx
@@ -0,0 +1,11 @@
+function App(){
+  return (
+    <div>
+      <h1>My name is Amitanshu Varshney</h1>
+      <h3>I have gained some confidence after clearing the app.jsx file and writing the small function code on my own</h3>
+      <h4>Ab ye mera khud ka project hai</h4>
+    </div>
+  )
+}
+
+export default App
